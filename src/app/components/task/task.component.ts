dbdb246e78a8fea8b08406bfb647d0bde1927fa5
James Sinkler <jamessinkler713@gmail.com>
2021-09-16 12:58:43 -0700

    use font-awesome fonts

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Task} from '../../mock-tasks'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: [  './task.component.css']
 })
 export class TaskComponent implements OnInit {
   @Input()
   task!: Task;
+  faTimes = faTimes;
 
   constructor() { }
 
