3897376a42deaa9b494116273a85194c43a86a9c
James Sinkler <jamessinkler713@gmail.com>
2021-09-19 14:34:08 -0700

    working with server and http module

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+// http client instead of fetch, update app.module to bring it in as well
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 // observables for async data
 import { of, Observable } from 'rxjs'
 import { TASKS, Task } from '../mock-tasks';
@@ -7,12 +9,17 @@ import { TASKS, Task } from '../mock-tasks';
   providedIn: 'root'
 })
 export class TaskService {
+  // our fake backend api
+  private apiUrl = 'http://localhost:3000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // our service method to get TASKS
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks
+    //const tasks = of(TASKS)
+    //return tasks
+
+    // using the server
+    return this.http.get<Task[]>(this.apiUrl)
   }
 }
